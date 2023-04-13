2f2ba5e69f3f465f00c67ad39e158bb952e0ac77
MehbubRashid <mehbub.rabu@gmail.com>
2023-04-13 14:05:13 +0600

    trying to show popup html content

--- assets/src/js/public.js
+++ assets/src/js/public.js
@@ -2,8 +2,8 @@
     "use strict";
 
     $(document).ready(function(){
-        // add edit button on the bottom of visible attributes on the cart item
-        $('.woocommerce-cart-form__cart-item .variation').append('<div class="uvcw-edit"><i class="dashicons dashicons-edit"></i>'+uvcw.edit+'</div>');
+        
+
     });
 
     var textToHTML= function (str) {
@@ -15,31 +15,20 @@
     };
 
     $(document).on('click', '.uvcw-edit', function(){
-        var product_url = $(this).closest('.woocommerce-cart-form__cart-item').find('.product-name a').attr('href');
+        var htmlContent = $(this).closest('.woocommerce-cart-form__cart-item').find('.uvcw-popup-source').text();
 
         Swal.fire({
-            html: `
-            <div class="uvcw-swal-content">
-                <div class="uvcw-swal-loader">
-                    <div class="uvcw-spinner"></div>
-                </div>
-            </div>
-            `,
+            html: textToHTML(htmlContent),
             showClass: {
                 popup: 'uvcw-swal'
             },
             didOpen: function(){
-                if ( product_url ) {
-                    $.get(product_url, function(response){
-                        if ( response ) {
-                            $('.uvcw-swal-loader').hide();
-                            $('.uvcw-swal-content').append(textToHTML(response));
-                        }
-                    });
-                }
+                
             },
             showConfirmButton: false,
             showCloseButton: true,
+            scrollbarPadding: false,
+            target: '.site-content'
         })
     });
 
--- includes/class-plugin.php
+++ includes/class-plugin.php
@@ -122,11 +122,77 @@ class Uvcw_Plugin {
 		add_action( 'wp_enqueue_scripts', array( $this, 'public_scripts' ) );
 
 		add_filter( 'woocommerce_get_item_data', array($this, 'variation_data_html'), 10, 2 );
+
+		add_action( 'woocommerce_after_template_part', array($this, 'after_wc_template'), 10, 4 );
+	}
+
+	// add edit button
+	public function after_wc_template($template_name, $template_path, $located, $args) {
+		if ( $template_name === 'cart/cart-item-data.php' ) {
+			?> 
+			<div class="uvcw-edit">
+				<i class="dashicons dashicons-edit"></i>
+				<?php echo esc_html__('Edit', 'update-variation-cart-woocommerce'); ?>
+			</div>
+			<?php
+		}
 	}
 
 
 	public function variation_data_html($item_data, $cart_item) {
-		// echo json_encode($item_data);
+		$product_id = $cart_item['product_id'];
+		global $product, $post;
+		$post = get_post($product_id);
+		$product = wc_get_product($product_id);
+		
+		?> 
+		<div class="uvcw-popup-source">
+			<?php ob_start(); ?>
+			<div class="uvcw-product-container product">
+				<?php 
+				$images = array(get_post_thumbnail_id( $product_id ));
+				$gallery_images = $product->get_gallery_image_ids();
+				if ( !is_array($gallery_images) ) {
+					$gallery_images = array();
+				}
+
+				$images = array_unique(array_merge($images, $gallery_images));
+
+				
+				if ( count( $images ) ) {
+					?> 
+					<div class="uvcw-prod-images">
+						<?php 
+						foreach ($images as $attachment_id) {
+							$url = wp_get_attachment_image_src( $attachment_id, 'full' );
+							if ( is_array($url) ) {
+								$url = $url[0];
+							}
+							?> 
+							<a target="_blank" href="<?php echo esc_url( $url ); ?>">
+								<img src="<?php echo esc_url($url); ?>" alt="">
+							</a>
+							<?php
+						}
+						?>
+					</div>
+					<?php
+				}
+				?>
+				
+				<div class="uvcw-prod-details">
+					<h1 class="uvcw-prod-title">
+						<a target="_blank" href="<?php echo esc_url( get_the_permalink( $product_id ) ); ?>">
+							<?php echo esc_html($product->get_name()); ?>
+						</a>
+					</h1>
+					<?php wc_get_template( 'single-product/price.php' ); ?>
+					<?php do_action( 'woocommerce_' . $product->get_type() . '_add_to_cart' ); ?>
+				</div>
+			</div>
+			<?php echo esc_html(ob_get_clean()); ?>
+		</div>
+		<?php
 		return $item_data;
 	}
 
@@ -137,11 +203,10 @@ class Uvcw_Plugin {
 	 */
 	public function public_scripts() {
 		if ( function_exists('is_cart') && is_cart() ) {
+			wp_enqueue_script( 'wc-add-to-cart' );
+			wp_enqueue_script( 'wc-add-to-cart-variation' );
 			wp_enqueue_script( 'sweetalert2', UVCW_ASSETS_URL . 'js/sweetalert2.min.js', array('jquery'), UVCW_ASSETS_VERSION, true );
 			wp_enqueue_script( 'update-variation-cart-woocommerce-public', UVCW_ASSETS_URL . 'dist/js/public.min.js', array( 'jquery' ), UVCW_ASSETS_VERSION, true );
-			wp_localize_script( 'update-variation-cart-woocommerce-public', 'uvcw', array(
-				'edit' => esc_html__('Edit', 'update-variation-cart-woocommerce')
-			) );
 			wp_enqueue_style( 'update-variation-cart-woocommerce-public', UVCW_ASSETS_URL . 'dist/css/public.min.css', array(), UVCW_ASSETS_VERSION );
 		}
 	}
